cf0a8f259f5ef4a05954b59c43e32e9e4ddfd018
nmgrluser <jan@argonlab7s-Mac-mini.local>
2021-04-09 16:28:16 -0600

    repo updated for analysis 27349-07A

--- scripts/jan/jan_unknown600_180_NC.py
+++ scripts/jan/jan_unknown600_180_NC.py
@@ -3,7 +3,7 @@
 baseline:
   after: true
   before: false
-  counts: 120
+  counts: 600
   detector: H1
   mass: 34.2
   settling_time: 15
@@ -15,7 +15,7 @@ equilibration:
   outlet: O
   use_extraction_eqtime: true
 multicollect:
-  counts: 600
+  counts: 900
   detector: H1
   isotope: Ar40
 peakcenter:
