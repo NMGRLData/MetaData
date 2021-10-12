a66420f2d4cad999d1dd93bb7a43078579a4156c
nmgrluser <jan@argonlab7s-Mac-mini.local>
2021-10-12 08:11:04 -0600

    repo updated for analysis 69166-03

--- scripts/jan/jan_unknown400_120_no_center.py
+++ scripts/jan/jan_unknown400_120_no_center.py
@@ -3,18 +3,18 @@
 baseline:
   after: true
   before: false
-  counts: 60
+  counts: 45
   detector: H1
   mass: 34.2
   settling_time: 15.0
-default_fits: nominal
+default_fits: nominal_linear
 equilibration:
   eqtime: 1.0
   inlet: R
   inlet_delay: 3
   outlet: O
   use_extraction_eqtime: true
-  post_equilibration_delay: 0
+  post_equilibration_delay: 3
 multicollect:
   counts: 400
   detector: H1
@@ -26,31 +26,28 @@ peakcenter:
   detectors:
   - H1
   - AX
-  - L2
   - CDD
-  integration_time: 0.262144
   isotope: Ar40
+  integration_time: 0.262144
 peakhop:
-  generate_ic_table: false
   hops_name: ''
-  ncycles: 0
   use_peak_hop: false
 '''
 ACTIVE_DETECTORS=('H2','H1','AX','L1','L2','CDD')
-    
+
 def main():
     info('unknown measurement script')
-    
+
     activate_detectors(*ACTIVE_DETECTORS)
-   
-    
+
+
     if mx.peakcenter.before:
         peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope)
-    
+
     if mx.baseline.before:
         baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector,
                   settling_time=mx.baseline.settling_time)
-    
+
     position_magnet(mx.multicollect.isotope, detector=mx.multicollect.detector)
 
     #sniff the gas during equilibration
@@ -62,13 +59,11 @@ def main():
     Equilibrate is non-blocking so use a sniff or sleep as a placeholder
     e.g sniff(<equilibration_time>) or sleep(<equilibration_time>)
     '''
-
-    equilibrate(eqtime=eqt, inlet=mx.equilibration.inlet, outlet=mx.equilibration.outlet, 
+    equilibrate(eqtime=eqt, inlet=mx.equilibration.inlet, outlet=mx.equilibration.outlet,
                delay=mx.equilibration.inlet_delay)
-
     set_time_zero()
-    
-    sniff(eqt)    
+
+    sniff(eqt)
     set_fits()
     set_baseline_fits()
 
@@ -77,19 +72,17 @@ def main():
 
     #multicollect on active detectors
     multicollect(ncounts=mx.multicollect.counts, integration_time=1)
-    
+
     if mx.baseline.after:
-        baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector, 
+        baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector,
                   settling_time=mx.baseline.settling_time)
     if mx.peakcenter.after:
         activate_detectors(*mx.peakcenter.detectors, **{'peak_center':True})
         peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope,
-        integration_time=mx.peakcenter.integration_time)
+                    integration_time=mx.peakcenter.integration_time)
     else:
         position_magnet(mx.multicollect.isotope, detector=mx.multicollect.detector, for_collection=False)
-
     if use_cdd_warming:
-       gosub('warm_cdd', argv=(mx.equilibration.outlet,))    
-       
+       gosub('warm_cdd', argv=(mx.equilibration.outlet,))
+
     info('finished measure script')
-    
