3be97f900df4c33d511a2582603a27bc2d68dc81
nmgrluser <jan@argonlab7s-Mac-mini.local>
2022-03-02 13:50:33 -0700

    repo updated for analysis c-03-J-4617

--- scripts/jan/jan_cocktail_x1.py
+++ scripts/jan/jan_cocktail_x1.py
@@ -42,6 +42,8 @@ def main():
             open('P')
             open('L')
             open('T')
+            open('Q')
+            open('M')
 
             close(description='Bone to Minibone')
             open(description='Minibone to Bone')         
