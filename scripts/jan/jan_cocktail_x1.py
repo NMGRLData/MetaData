2c4b61be38ea26623b26a352c687347af1010e9e
nmgrluser <jan@argonlab7s-Mac-mini.local>
2022-03-02 09:56:41 -0700

    repo updated for analysis bc-03-J-1248

--- scripts/jan/jan_cocktail_x1.py
+++ scripts/jan/jan_cocktail_x1.py
@@ -15,6 +15,8 @@ def main():
     #sleep(duration=2.0)
     #close(name="M", description="Microbone to Getter NP-10H")
     #sleep(duration=2.0)
+    close('T')
+    
     gosub('common:ExpandPipette1')
 
 #===============================================================================
@@ -28,12 +30,36 @@ def main():
         gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
         gosub('jan:PumpMiniboneAfterDiodeAnalysis')
     else:
-        gosub('jan:PumpMicrobone')
+        #gosub('jan:PumpMicrobone')
         v=get_resource_value(name='JanMiniboneFlag')
         info('get resource value {}'.format(v))
         if v:
             info('Pumping Minibone')
-            gosub('jan:PumpMinibone')
+
+            #close('I')
+            #close('C')
+            #close('Q')
+            open('P')
+            open('L')
+            open('T')
+
+            close(description='Bone to Minibone')
+            open(description='Minibone to Bone')         
+            #close(description='Microbone to Minibone')
+            
+            #sleep(duration=1.0)
+            open(description='Minibone to Turbo')
+            #open(description='Quad Inlet')
+            
+            open('S')
+
+            set_resource(name='MinibonePumpTimeFlag',value=30)
+            sleep(duration=15.0)
+            close(description="Outer Pipette 1")
+            close(description="Outer Pipette 2")
+            
+            release('JanMiniboneFlag')
+
         else:
             info('Not Pumping Minibone')
 
