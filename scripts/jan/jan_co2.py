42040fa67fcef6e744241f20051eb720e65d2435
nmgrluser <jan@argonlab7s-Mac-mini.local>
2022-03-02 10:08:39 -0700

    repo updated for analysis bu-FC-J-16019

--- scripts/jan/jan_co2.py
+++ scripts/jan/jan_co2.py
@@ -5,9 +5,11 @@
 eqtime: 15
 '''
 def main():
-    info('Jan CO2 laser analysis')
+    info('Jan CO2 laser analysis. Pump using minibone')
     gosub('jan:WaitForCO2Access')
-    gosub('jan:PrepareForCO2Analysis')
+    gosub('jan:WaitForMiniboneAccess')
+
+    gosub('jan:PrepareForCO2Analysis_minibone_pump')
     #sleep(duration=2.0)
     #close(name="M", description="Microbone to Getter NP-10H")
     #sleep(duration=2.0)
@@ -24,12 +26,36 @@ def main():
         gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
         gosub('jan:PumpMiniboneAfterDiodeAnalysis')
     else:
-        gosub('jan:PumpMicrobone')
+        #gosub('jan:PumpMicrobone')
         v=get_resource_value(name='JanMiniboneFlag')
         info('get resource value {}'.format(v))
         if v:
             info('Pumping Minibone')
-            gosub('jan:PumpMinibone')
+
+            #close('I')
+            #close('C')
+            #close('Q')
+            open('P')
+            open('L')
+            open('T')
+
+            close(description='Bone to Minibone')
+            open(description='Minibone to Bone')         
+            #close(description='Microbone to Minibone')
+            
+            #sleep(duration=1.0)
+            open(description='Minibone to Turbo')
+            #open(description='Quad Inlet')
+            
+            open('S')
+
+            set_resource(name='MinibonePumpTimeFlag',value=30)
+            sleep(duration=15.0)
+            close(description="Outer Pipette 1")
+            close(description="Outer Pipette 2")
+            
+            release('JanMiniboneFlag')
+
         else:
             info('Not Pumping Minibone')
 
