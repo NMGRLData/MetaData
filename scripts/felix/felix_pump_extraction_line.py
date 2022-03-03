a298c10202a129f347b2b7418b7ae8ad6a27be5f
felix <nmgrluser@gmail.com>
2022-03-03 09:52:19 -0700

    repo updated for analysis bc-03-F-498

--- scripts/felix/felix_pump_extraction_line.py
+++ scripts/felix/felix_pump_extraction_line.py
@@ -2,4 +2,5 @@ def main():
     info('Pump after analysis')
     gosub('felix:PumpBone')
     if get_resource_value(name='FelixMiniboneFlag'):
+        open('L')
         gosub('felix:PumpMinibone')
