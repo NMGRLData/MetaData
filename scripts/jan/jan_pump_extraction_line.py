e434f5c41466ad25e08a7d54936affb0fde7b90c
jan <jan@nmt.edu>
2022-05-16 10:40:29 -0600

    repo updated for analysis ba-03-J-669

--- scripts/jan/jan_pump_extraction_line.py
+++ scripts/jan/jan_pump_extraction_line.py
@@ -6,40 +6,11 @@ def main():
         gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
         gosub('jan:PumpMiniboneAfterDiodeAnalysis')
     else:
-        #gosub('jan:PumpMicrobone')
-        set_resource(name='CO2PumpTimeFlag', value=30)
-        release('JanCO2Flag')
-        
+        gosub('jan:PumpMicrobone')
         v=get_resource_value(name='JanMiniboneFlag')
         info('get resource value {}'.format(v))
         if v:
             info('Pumping Minibone')
-
-            #close('I')
-            #close('C')
-            #close('Q')
-            open('P')
-            open('L')
-            open('T')
-            open('Q')
-            open('M')
-
-            close(description='Bone to Minibone')
-            open(description='Minibone to Bone')         
-            #close(description='Microbone to Minibone')
-            
-            #sleep(duration=1.0)
-            open(description='Minibone to Turbo')
-            #open(description='Quad Inlet')
-            
-            open('S')
-
-            set_resource(name='MinibonePumpTimeFlag',value=30)
-            sleep(duration=15.0)
-            close(description="Outer Pipette 1")
-            close(description="Outer Pipette 2")
-            
-            release('JanMiniboneFlag')
-
+            gosub('jan:PumpMinibone')
         else:
             info('Not Pumping Minibone')
