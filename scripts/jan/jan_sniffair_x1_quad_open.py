37e999346e7db8fb9b942fb75312636db6a23354
jan <jan@nmt.edu>
2023-01-06 09:07:04 -0700

    repo updated for analysis a-03-J-2628

--- scripts/jan/jan_sniffair_x1_quad_open.py
+++ scripts/jan/jan_sniffair_x1_quad_open.py
@@ -12,7 +12,6 @@ def main():
     open(name="Q", description="Quad Inlet")
     gosub('jan:EvacPipette2')
     gosub('common:FillPipette2')
-    close('T')
     gosub('jan:PrepareForAirShotExpansion')
     gosub('common:SniffPipette2')
 
@@ -27,38 +26,12 @@ def main():
         gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
         gosub('jan:PumpMiniboneAfterDiodeAnalysis')
     else:
-        #gosub('jan:PumpMicrobone')
+        gosub('jan:PumpMicrobone')
         v=get_resource_value(name='JanMiniboneFlag')
         info('get resource value {}'.format(v))
         if v:
             info('Pumping Minibone')
-
-            #close('I')
-            #close('C')
-            #close('Q')
-            open('P')
-            open('L')
-            open('T')
-            open('Q')
-            open('M')
-
-            close(description='Bone to Minibone')
-            open(description='Minibone to Bone')         
-            #close(description='Microbone to Minibone')
-            
-            #sleep(duration=1.0)
-            open(description='Minibone to Turbo')
-            #open(description='Quad Inlet')
-            
-            open('S')
-
-            set_resource(name='MinibonePumpTimeFlag',value=30)
-            sleep(duration=15.0)
-            close(description="Outer Pipette 1")
-            close(description="Outer Pipette 2")
-            
-            release('JanMiniboneFlag')
-
+            gosub('jan:PumpMinibone')
         else:
             info('Not Pumping Minibone')
 
