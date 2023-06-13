62ea28f953a881ad79327f246dd83d72d609b3e6
jan <jan@nmt.edu>
2023-06-13 10:58:23 -0600

    repo updated for analysis ba-03-J-1190

--- scripts/jan/jan_sniffair_x1.py
+++ scripts/jan/jan_sniffair_x1.py
@@ -21,43 +21,17 @@ def main():
 def main():
     info('Pump after analysis')
 
-    if extract_device=="FusionsDiode":
+    if extract_device=="FusionsDiode" or extract_device=='NMGRLFurnace':
         info('Pump after Jan diode analysis')
         gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
         gosub('jan:PumpMiniboneAfterDiodeAnalysis')
     else:
-        #gosub('jan:PumpMicrobone')
+        gosub('jan:PumpMicrobone')
         v=get_resource_value(name='JanMiniboneFlag')
         info('get resource value {}'.format(v))
         if v:
             info('Pumping Minibone')
-
-            #close('I')
-            #close('C')
-            #close('Q')
-            open('P')
-            open('L')
-            open('T')
-            open('Q')
-            open('M')
-
-            close(description='Bone to Minibone')
-            open(description='Minibone to Bone')         
-            #close(description='Microbone to Minibone')
-            
-            #sleep(duration=1.0)
-            open(description='Minibone to Turbo')
-            #open(description='Quad Inlet')
-            
-            open('S')
-
-            set_resource(name='MinibonePumpTimeFlag',value=30)
-            sleep(duration=15.0)
-            close(description="Outer Pipette 1")
-            close(description="Outer Pipette 2")
-            
-            release('JanMiniboneFlag')
-
+            gosub('jan:PumpMinibone')
         else:
             info('Not Pumping Minibone')
 
