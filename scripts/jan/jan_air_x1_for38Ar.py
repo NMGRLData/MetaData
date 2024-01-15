d241113260d2c5fcad5145e03570acc280480d14
jan <jan@nmt.edu>
2024-01-15 10:09:53 -0700

    repo updated for analysis ba-03-J-1434

--- scripts/jan/jan_air_x1_for38Ar.py
+++ scripts/jan/jan_air_x1_for38Ar.py
@@ -9,14 +9,14 @@ def main():
     info('Jan Air Script x1')
     gosub('jan:WaitForMiniboneAccess')
     gosub('jan:PrepareForAirShot')
-    open('T')
-    open('Q')
+    #open('T')
+    close('Q')
     gosub('jan:EvacPipette2')
     gosub('common:FillPipette2')
     gosub('jan:PrepareForAirShotExpansion')
     gosub('common:ExpandPipette2')
     #close(name="T", description="Microbone to CO2 Laser")
-    close('L')
+    #close('L')
     sleep(3)
     
 
@@ -27,7 +27,7 @@ def main():
 def main():
     info('Pump after analysis')
 
-    if extract_device=="FusionsDiode":
+    if extract_device=="FusionsDiode" or extract_device=='NMGRLFurnace':
         info('Pump after Jan diode analysis')
         gosub('jan:PumpMicroBoneAfterDiodeAnalysis')
         gosub('jan:PumpMiniboneAfterDiodeAnalysis')
@@ -46,5 +46,5 @@ def main():
 #===============================================================================
 def main():
     info('Pumping spectrometer')
-    open(name='O')
+    open(name='O', cancel_on_failed_actuation=False)
     
