389d8fcc2ed3c3cd48a0f2c6f791551c86ce2754
jan <jan@nmt.edu>
2024-02-17 14:07:25 -0700

    repo updated for analysis ba-03-J-1499

--- scripts/jan/jan_SyncExt_unknown120_dynamic_no_center.py
+++ scripts/jan/jan_SyncExt_unknown120_dynamic_no_center.py
@@ -14,7 +14,7 @@ equilibration:
   inlet_delay: 3
   outlet: O
   use_extraction_eqtime: true
-  post_equilibration_delay: 5
+  post_equilibration_delay: 2
 multicollect:
   counts: 120
   detector: H1
