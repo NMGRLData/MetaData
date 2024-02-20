e358efb501c57eac181ed297586a6287d64d901c
jan <jan@nmt.edu>
2024-02-20 10:15:02 -0700

    repo updated for analysis 71467-12B

--- scripts/jan/jan_pump_ms.py
+++ scripts/jan/jan_pump_ms.py
@@ -2,8 +2,8 @@ def main():
     info('Pumping spectrometer')
     open(name='O', cancel_on_failed_actuation=False)
     
-    
+    # setup dynamic pumping
     # delay extra time of the ar40 intensity is greater than a set threshold
     #ar40intensity = get_intensity('H1')
-    #if ar40intensity> 1000:
+    #if ar40intensity> 4900:
     #    sleep(20)
