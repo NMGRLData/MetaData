cd5da2a4ea7ab118478b73a8e2c52c7c014aaf19
jan <jan@nmt.edu>
2024-02-21 11:42:55 -0700

    repo updated for analysis 29699-08

--- scripts/jan/jan_co2.py
+++ scripts/jan/jan_co2.py
@@ -40,8 +40,11 @@ def main():
     info('Pumping spectrometer')
     open(name='O', cancel_on_failed_actuation=False)
     
-    #setup dynamic pumping
+    # setup dynamic pumping
     # delay extra time of the ar40 intensity is greater than a set threshold
-    #ar40intensity = get_intensity('H1')
-    #if ar40intensity> 4900:
-    #sleep(20)
+    
+    ar40intensity = get_intensity('H1')
+    if ar40intensity> 4900:
+        sleep(20)
+    elif ar40intensity>3000:
+        sleep(10)
