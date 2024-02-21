159fb673ac4ea8a0bb56c8042e0cc35cef5649ab
jan <jan@nmt.edu>
2024-02-21 14:29:48 -0700

    repo updated for analysis c-03-J-7411

--- scripts/jan/jan_cocktail_x1.py
+++ scripts/jan/jan_cocktail_x1.py
@@ -49,7 +49,7 @@ def main():
     # delay extra time of the ar40 intensity is greater than a set threshold
     
     ar40intensity = get_intensity('H1')
-    if ar40intensity> 600:
-        sleep(100)
+    if ar40intensity> 4900:
+        sleep(20)
     elif ar40intensity>3000:
         sleep(10)
