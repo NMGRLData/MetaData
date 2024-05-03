8d1154210629389bebdcadd995e16074debc197a
jan <jan@nmt.edu>
2024-05-03 06:55:48 -0600

    repo updated for analysis bu-FC-J-19339

--- scripts/jan/jan_SyncExt_600_NC.py
+++ scripts/jan/jan_SyncExt_600_NC.py
@@ -14,7 +14,7 @@ equilibration:
   inlet_delay: 3
   outlet: O
   use_extraction_eqtime: true
-  post_equilibration_delay: 2
+  post_equilibration_delay: 5
 multicollect:
   counts: 600
   detector: H1
