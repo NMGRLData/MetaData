2a63da2ef9ec74951a21bc9214ed1e39328e0053
jan <jan@jan2.local>
2024-08-21 08:29:17 -0600

    repo updated for analysis a-03-J-4383

--- scripts/jan/jan_air_x1.py
+++ scripts/jan/jan_air_x1.py
@@ -10,7 +10,7 @@ def main():
     gosub('jan:WaitForMiniboneAccess')
     gosub('jan:PrepareForAirShot')
     gosub('jan:EvacPipette2')
-    open(name="T", description="Microbone to CO2 Laser")
+    close(name="T", description="Microbone to CO2 Laser")
     open(name="Q", description="Quad Inlet")
     gosub('common:FillPipette2')
     close(name="M", description="Microbone to Getter NP-10H")
