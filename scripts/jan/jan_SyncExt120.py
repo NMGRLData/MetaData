67e406b62231873afbb63453c2556004afc29ab3
jan <jan@jan2.local>
2024-08-21 11:02:40 -0600

    repo updated for analysis c-03-J-7908

--- scripts/jan/jan_SyncExt120.py
+++ scripts/jan/jan_SyncExt120.py
@@ -14,7 +14,7 @@ equilibration:
   inlet_delay: 3
   outlet: O
   use_extraction_eqtime: true
-  post_equilibration_delay: 6
+  post_equilibration_delay: 5
 multicollect:
   counts: 120
   detector: H1
