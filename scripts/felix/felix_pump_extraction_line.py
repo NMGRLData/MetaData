ab7c3b4d2ffd4526ea64ebd4432ce1f805327a2c
hornblende <hornblende@hornblende.local>
2026-04-16 10:06:04 -0600

    repo updated for analysis bu-FD-F-5894

--- scripts/felix/felix_pump_extraction_line.py
+++ scripts/felix/felix_pump_extraction_line.py
@@ -1,2 +1,6 @@
 def main():
-    info('Do not pump extraction line after analysis')
+    info('Do not pump extraction after analysis')
+    #info('Pump extraction after analysis')
+    # gosub('felix:PumpBone')
+    # if get_resource_value(name='FelixMiniboneFlag'):
+    #     gosub('felix:PumpMinibone')
