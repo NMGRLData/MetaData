30e2807bf63b4ff3d07ad112fa451987aec71d37
felix <nmgrluser@gmail.com>
2026-07-01 16:45:56 -0600

    repo updated for analysis bu-FC-F-61

--- scripts/felix/felix_do_not_pump_extraction_line.py
+++
@@ -1,6 +0,0 @@
-def main():
-    info('Do not pump extraction after analysis')
-    #info('Pump extraction after analysis')
-    # gosub('felix:PumpBone')
-    # if get_resource_value(name='FelixMiniboneFlag'):
-    #     gosub('felix:PumpMinibone')
