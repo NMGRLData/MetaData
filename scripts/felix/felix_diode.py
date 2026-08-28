7bf125d3a08088fdef8b5785de70b8b562bce16e
hornblende <hornblende@hornblende.local>
2026-08-28 12:11:22 -0600

    repo updated for analysis 72909-04ET

--- scripts/felix/felix_diode.py
+++ scripts/felix/felix_diode.py
@@ -9,29 +9,29 @@ modifier: 01
 def main():
 
     info('Felix unknown laser analysis')
-
+    
     set_motor('beam',beam_diameter)
-
+    
     gosub('felix:PrepareForDiodeAnalysis')
 
 
-    close(name="C", description="Bone to Turbo")
+    close(name="C", description="Bone to Turbo")  
     close(name="I", description="Minibone to Bone")
     close(name="D", description="Bone to CO2 Laser")
     close(name="E", description="Bone to Minibone")
 
-
+ 
     gosub('felix:IsolateDiodeColdfinger')
 
     #open(name="B", description="Bone to Diode Laser")
 
-
+    
     '''
     keep pumping bone while cold finger isolated
     '''
-
+    
     #open(description='Bone to Turbo')
-
+  
     if analysis_type=='blank':
         info('Blank Analyis. No laser heating')
 
@@ -53,10 +53,10 @@ def main():
         with video_recording('{}/{}'.format(load_identifier, run_identifier)):
             enable()
             for pi in position:
-                '''
+                ''' 
                 position the laser at pi, pi can be an holenumber or (x,y)
                 '''
-                with lighting(65):
+                with lighting(55):
                     sleep(2)
                     move_to_position(pi)
                     sleep(2)
@@ -66,32 +66,32 @@ def main():
                     extract(0)
             info('Diode laser disabled.')
             disable()
-
-    gosub('felix:EquilibrateThenIsolateDiodeColdfinger')
+      
+    gosub('felix:EquilibrateThenIsolateDiodeColdfinger')    
     #open(name="B", description="Bone to Diode Laser")
 
-
+    
     sleep(cleanup)
 
 def do_extraction():
-
+    
     info('begin interval')
     begin_interval(duration)
-
+    
     if ramp_duration>0:
         info('ramping to {} at {}/s'.format(extract_value, ramp_rate, extract_units))
         ramp(setpoint=extract_value, duration=ramp_duration, period=0.5)
     else:
         extract(extract_value, extract_units)
-
+    
     if pattern:
         info('executing pattern {}'.format(pattern))
         execute_pattern(pattern)
-
+        
     complete_interval()
-
+    
 #def do_extraction():
-#
+#    
 #    if ramp_rate>0:
 #        '''
 #        style 1.
@@ -109,7 +109,7 @@ def do_extraction():
 #
 #    else:
 #        begin_interval(duration)
-#
+#        
 #        info('set extract to {} ({})'.format(extract_value, extract_units))
 #        extract()
 #        sleep(2)
@@ -119,6 +119,7 @@ def do_extraction():
 #            execute_pattern(pattern)
 #
 #        complete_interval()
+
 
 #===============================================================================
 # POST EQUILIBRATION SCRIPT felix_pump_extraction_line.py
